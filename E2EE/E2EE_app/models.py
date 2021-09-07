261528b136f34ef3b093c8c11db12d93dcd5369e
ardalangh <ardygh@berkeley.edu>
2021-09-07 15:06:48 -0700

    password_argon_hash added

--- E2EE/E2EE_app/models.py
+++ E2EE/E2EE_app/models.py
@@ -1,3 +1,10 @@
+import argon2
 from django.db import models
 
 
+class User(models.Model):
+
+
+
+
+    user_uuid =
--- E2EE/E2EE_app/views.py
+++ E2EE/E2EE_app/views.py
@@ -1,5 +1,28 @@
+import argon2
 from django.shortcuts import render
 
 
 def signup(req):
     return render(req, 'signup.html')
+
+
+def get_argon_key(password, salt, argon_hash_len):
+    return argon2.low_level.hash_secret(  # return: bytes
+        bytes(password, 'ascii'),  # pass
+        bytes(salt, 'ascii'),  # salt
+        time_cost=1,
+        memory_cost=64 * 1024,
+        parallelism=4,
+        hash_len=argon_hash_len,
+        type=argon2.low_level.Type.D
+    )
+
+
+def process_signup(req):
+    if req.method == 'POST':
+        user = req.POST.get("username")
+        password = req.POST.get("username")
+        if user and password:
+            argon_hash_len = 64
+
+            password_argon_hash = get_argon_key(password, user, argon_hash_len)
