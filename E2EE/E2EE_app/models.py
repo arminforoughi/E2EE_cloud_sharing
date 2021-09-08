450445361564cf64369df466f89cdce953f76936
ardalangh <ardygh@berkeley.edu>
2021-09-07 20:41:27 -0700

    model added

--- E2EE/E2EE_app/models.py
+++ E2EE/E2EE_app/models.py
@@ -2,9 +2,8 @@ import argon2
 from django.db import models
 
 
-class User(models.Model):
+class Public_Key(models.Model):
+    username = models.CharField(max_length= 450)
+    public_key = models.CharField(max_length= 450)
 
 
-
-
-    user_uuid =
--- E2EE/E2EE_app/views.py
+++ E2EE/E2EE_app/views.py
@@ -1,12 +1,21 @@
 import hashlib
 import hmac
 import uuid
+from Crypto.PublicKey import RSA
 import argon2
 from django.shortcuts import render
+
+from E2EE.E2EE_app.models import *
 
 
 def signup(req):
     return render(req, 'signup.html')
+
+
+def get_pke_key(): # returns: RSA key object -- use .e for public & .d for private
+    return RSA.generate(2048)
+
+
 
 
 def get_argon_key(password, salt, argon_hash_len):
@@ -24,10 +33,36 @@ def get_uuid(password_argon_hash, username):
     return uuid.UUID(hmac.new(password_argon_hash, username, hashlib.sha256))
 
 
+
+
+
+
+def init_user(username, password):
+    rsa_key = RSA.generate(2048)
+
+    # store username + rsa_public_key in Public_Key DB
+    Public_Key(username, rsa_key.publickey().exportKey()).save()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def process_signup(req):
     if req.method == 'POST':
         user = req.POST.get("username")
-        password = req.POST.get("username")
+        password = req.POST.get("password")
         if user and password:
             argon_hash_len = 32
             password_argon_hash = get_argon_key(password, user, argon_hash_len)
