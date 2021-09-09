022c406b0b69c222bdc1ec6d1832f0c36c7b6757
ardalangh <ardygh@berkeley.edu>
2021-09-08 20:47:22 -0700

    test_sym_enc added and passed

--- E2EE/E2EE_app/tests.py
+++ E2EE/E2EE_app/tests.py
@@ -1,3 +1,23 @@
+import json
+import unittest
+
+from Crypto.Cipher import AES
 from django.test import TestCase
 
-# Create your tests here.
+from E2EE.E2EE_app.views import get_random_bytes, sym_enc
+
+
+class TestSym(unittest.TestCase):
+
+    def test_sym_enc(self):
+        data = {"name": "ardy", "key": "somekey"}
+        key = get_random_bytes(AES.block_size)
+        iv = get_random_bytes(AES.block_size)
+        output = sym_enc(key, iv, data)
+        self.assertIsInstance(output, dict)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
